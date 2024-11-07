9fd6e580c41bf42ee7e500d2e2f3ec693033eecd
AmirAR <amir.rouzbeh@outlook.com>
2024-11-07 22:51:48 +0330

    ChangeAdminDashboardName

---
+++ Mandena.Dashboard/Helper/Services/CategoryService.cs
@@ -0,0 +1,19 @@
+﻿using Mandena.Application.DTOs.CategoriesDtos;
+using System.Net.Http.Json;
+
+namespace Mandena.Dashboard.Helper.Services
+{
+    public class CategoryService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<CategoryDTO>> GetCategories()
+        {
+            return await _httpClient.GetFromJsonAsync<List<CategoryDTO>>("Catrgory"); // URL API برای دریافت محصولات
+        }
+    }
+}
--- Mandena.Dashboard/Program.cs
+++ Mandena.Dashboard/Program.cs
@@ -1,17 +1,14 @@
-using Mandena.AdminDashboard;
-using Mandena.AdminDashboard.Helper;
-using Mandena.AdminDashboard.Helper.Services;
+using Mandena.Dashboard;
+using Mandena.Dashboard.Helper.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using static System.Net.WebRequestMethods;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7056/api/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7056/api/") });
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<CategoryService>();
-
 await builder.Build().RunAsync();
